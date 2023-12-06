97c5b10d19f5bdaec44a4899ef716d82e8cf260d
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-12-06 21:54:41 +0500

    Add extra test for ship hitting functionality

--- test/ship.test.js
+++ test/ship.test.js
@@ -1,10 +1,20 @@
 import { Ship } from "../src/ship";
 
 describe("Ship class", () => {
-  test("Hit functionality works", () => {
+  test("Hit functionality works (1)", () => {
     const ship = new Ship(1);
     ship.hit();
     expect(ship.timesHit).toBe(1);
+  });
+
+  test("Hit functionality works (2)", () => {
+    const ship = new Ship(4);
+    ship.hit();
+    expect(ship.timesHit).toBe(1);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.timesHit).toBe(4);
   });
 
   test("Sinking detection works", () => {
